83b647e3f6ae58a9ec761349cbf768e1a8d4e9c4
Antonio Furtado <antoniordf@hotmail.com>
2023-01-18 20:26:42 -0300

    Refactored App function into a class and implemented functionality to record state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAitamgr_GysMuZ9seJrbWntNh2InkTFPE';
 
+
+
 // Create a new component. This component should produce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // the callback function could be refactored to: (videos) => {this.setState({ videos: videos })}
+    // It could be refactored further as such: (videos) => this.setState({ videos })
+    // when you have "videos: videos" you can just say "videos" in ES6.
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render() {
+      return (
+        <div>
+          <SearchBar />
+        </div>
+      );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
